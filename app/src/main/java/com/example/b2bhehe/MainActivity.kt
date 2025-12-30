065e97ffb9f1927d3fb7a02fa889f4a5512ae989
Christian Rivera <christianrivera488@gmail.com>
2025-12-30 01:01:43 -0500

    Experimented with more functions and updating info

--- app/src/main/java/com/example/b2bhehe/MainActivity.kt
+++ app/src/main/java/com/example/b2bhehe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.b2bhehe
 
+import android.R
 import android.R.attr.onClick
 import android.R.attr.button
 import android.R.attr.text
@@ -8,6 +9,9 @@ import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,10 +26,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.b2bhehe.ui.theme.B2bHEHETheme
 import androidx.compose.material3.Button
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -48,7 +62,7 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                     //alignment is for children only
-                    Box(modifier = Modifier
+                    /*Box(modifier = Modifier
                         .fillMaxSize() //take all the space willing to give/get, only what parent allows
                     ) { //fills whole screen
                         //padding is just how much extra space between a certain function, avoid overlap
@@ -56,10 +70,21 @@ class MainActivity : ComponentActivity() {
                             .align(Alignment.Center) //starts out centered
                             .offset(y = (-80).dp)
                         )
+                        topRightCounter(modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .offset(y = 50.dp)
+                        )
                         //for movement, start = right, top = down, end = left, bottom = up
                         //offset is visual shift, padding is layout space, offset works with x y
                         //negative up/left
+                    } */
+                    Box(modifier = Modifier
+                        .fillMaxSize()){
+                        centeredBox(modifier = Modifier
+                            .align(Alignment.Center)
+                        )
                     }
+
                 }
             }
         }
@@ -69,7 +94,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "C is for Calc",
         modifier = modifier
     )
 }
@@ -89,8 +114,46 @@ fun Header(modifier: Modifier = Modifier){
 @Composable
 fun PressIt(modifier: Modifier = Modifier){
     Button(onClick = {
-
+        counter++
     }, modifier = modifier) { //always make sure your function is actually using it
         Text("Press Me")
     }
 }
+
+@Composable
+fun topRightCounter(modifier: Modifier = Modifier){
+    Text(
+        counter.toString(),
+        modifier = modifier
+    )
+}
+
+fun getCount() : Int{
+    return counter
+}
+@Composable
+fun centeredBox(modifier: Modifier = Modifier){
+    Card(modifier = modifier //be sure to reference the actual modifer called, captilization
+        .width(260.dp)
+        .height(550.dp),
+        border = BorderStroke(2.dp, Color.Black)
+    ){
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(Color.Black)
+        ){
+            PressIt(modifier = modifier
+                .align(Alignment.Center)
+            )
+        }
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .border(2.dp, Color.Black)
+        )
+    }
+}
+var counter by mutableStateOf(0)
+//there are two types to keep track of info. remember and mutableStateOf, mutatable state of just
+//keeps track of info to know for the compostible to update
